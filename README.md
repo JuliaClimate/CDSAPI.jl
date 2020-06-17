4d3ce25224d18e157b2cbdf6b6534d5cacd34b1b
michiboo <chanmickyyun@gmail.com>
2020-06-17 09:43:45 +0300

    add base code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # CDSAPI
+Example 
+
+name = "reanalysis-era5-single-levels"
+params = Dict(
+       "variable"=> "2t",
+       "product_type"=> "reanalysis",
+       "date"=> "2015-12-01",
+       "time"=> "14:00",
+       "format"=> "netcdf",
+)
+download(res["location"], "get_data.nc")
--- src/CDSAPI.jl
+++ src/CDSAPI.jl
@@ -1,35 +1,22 @@
-module CDSAPI
 
-import HTTP
-import JSON
+module CDSAPI
+using HTTP, JSON, Base64
 # Write your package code here.
-    name = "reanalysis-era5-single-levels"
-    cred = Dict()
-    open("./src/c.json", "r") do f
-       dicttxt = read(f,String)
-       global cred
-       cred = JSON.parse(dicttxt)
-       end
-    params = Dict(
-        "variable"=> "temperature",
-        "pressure_level"=> "1000",
-        "product_type"=> "reanalysis",
-        "year"=> "2008",
-        "month"=> "01",
-        "day"=> "01",
-        "time"=> "12:00",
-        "format"=> "grib"
-    )
-    println(string(cred["url"], "/resources/$name"))
-    a = split(cred["key"],":")
-    a = (a[1], a[2])
-    println(string("Basic ", cred["key"]))
-    r = HTTP.request("GET", string(cred["url"], "/resources/$name"), ["Content-Type" => "application/json",  "Authorization" =>string("Basic ", cred["key"])], JSON.json(params))
-    println(r.body)
-    str = string(r.body)
-    resp_json = JSON.Parser.parse(str)
-    # write the file with the stringdata variable information
-    open("write_read.json", "w") do f
-        write(f, str)
-     end
-end
+    function retrieve(name, params)
+        cred = Dict()
+        open("c.json", "r") do f
+            dicttxt = read(f,String)
+            cred = JSON.parse(dicttxt)
+        end
+        key = string("Basic ", base64encode(cred["key"]))
+        r = HTTP.request("POST", string(cred["url"], "/resources/$name"), ["Authorization" => key], body=JSON.json(params), verbose=1) 
+        str = String(r.body)
+        resp_json = JSON.Parser.parse(str)
+        data = Dict("state" => "queued")
+        while data["state"] != "completed"
+            data = HTTP.request("GET", string(cred["url"], "/tasks/", resp_json["request_id"]),  ["Authorization" => key])
+            data = JSON.Parser.parse(String(data.body)) 
+        end
+        return data
+    end # function
+end # module
