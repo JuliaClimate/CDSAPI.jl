8eb07997cdab23d61f6e1eecaea0623f6a96952f
Lakshya Khatri <lakshyak67@gmail.com>
2020-06-30 17:25:24 +0530

    Add docstrings for py2ju() and retrieve()

--- src/CDSAPI.jl
+++ src/CDSAPI.jl
@@ -7,7 +7,16 @@ using Base64
 export
     py2ju
 
-function retrieve(name, params, filename)
+"""
+    retrieve(name::AbstractString,
+        params::Dict{T, V},
+        filename::AbstractString)
+
+Retrieves data for `name` from the Climate Data Store
+with the specified `params` and stores it in the current
+directory as `filename`.
+"""
+function retrieve(name::AbstractString, params::Dict, filename::AbstractString)
     creds = Dict()
     open(joinpath(homedir(),".cdsapirc")) do f
         for line in readlines(f)
@@ -33,10 +42,34 @@ function retrieve(name, params, filename)
     end
 
     download(data["location"], filename)
-
-    data
+    return data
 end
 
-py2ju(dictstr::String) = JSON.parse(dictstr)
+"""
+    py2ju(dictstr::AbstractString)
+
+Takes a Python dictionary as string and converts it into Julia's `Dict`
+
+# Examples
+```julia-repl
+julia> str = \"""{
+               "format": "zip",
+               "variable": "surface_air_temperature",
+               "product_type": "climatology",
+               "month": "08",
+               "origin": "era_interim"
+           }\""";
+
+julia> py2ju(str)
+Dict{String,Any} with 5 entries:
+  "format"       => "zip"
+  "month"        => "08"
+  "product_type" => "climatology"
+  "variable"     => "surface_air_temperature"
+  "origin"       => "era_interim"
+
+```
+"""
+py2ju(dictstr::AbstractString) = JSON.parse(dictstr)
 
 end # module
