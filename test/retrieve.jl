b5f89fbb42bfbe54ab3d6a26fb1f479996163626
Lakshya Khatri <lakshyak67@gmail.com>
2020-07-08 19:46:30 +0530

    Add tests for Europe water quantity data

--- test/retrieve.jl
+++ test/retrieve.jl
@@ -1,4 +1,6 @@
 using GRIB
+using ZipFile
+using NetCDF
 
 @testset "Retrieve" begin
     datadir = joinpath(@__DIR__,"data")
@@ -7,18 +9,18 @@ using GRIB
         filepath = joinpath(datadir, "era5.grib")
         response = CDSAPI.retrieve("reanalysis-era5-pressure-levels-monthly-means",
             py2ju("""{
-                    'format': 'grib',
-                    'product_type': 'monthly_averaged_reanalysis',
-                    'variable': 'divergence',
-                    'pressure_level': '1',
-                    'year': '2020',
-                    'month': '06',
-                    'area': [
-                        90, -180, -90,
-                        180,
-                    ],
-                    'time': '00:00',
-                }"""),
+                'format': 'grib',
+                'product_type': 'monthly_averaged_reanalysis',
+                'variable': 'divergence',
+                'pressure_level': '1',
+                'year': '2020',
+                'month': '06',
+                'area': [
+                    90, -180, -90,
+                    180,
+                ],
+                'time': '00:00',
+            }"""),
             filepath)
 
         @test typeof(response) <: Dict
@@ -33,5 +35,40 @@ using GRIB
             @test data["month"] == 6
         end
         rm(filepath)
+    end
+
+    @testset "Europe water quantity data" begin
+        filepath = joinpath(datadir, "ewq.zip")
+        response = CDSAPI.retrieve("sis-water-quantity-swicca",
+            py2ju("""{
+                'variable': 'river_flow',
+                'time_aggregation': 'annual_maximum',
+                'horizontal_aggregation': 'catchments',
+                'emissions_scenario': 'rcp_2_6',
+                'period': '2071_2100',
+                'return_period': '100',
+                'format': 'zip',
+            }"""),
+            filepath)
+
+        @test typeof(response) <: Dict
+        @test response["content_type"] == "application/zip"
+        @test isfile(filepath)
+
+        # extract contents
+        zip_reader = ZipFile.Reader(filepath)
+        ewq_fileio = zip_reader.files[1]
+        ewq_file = joinpath(datadir, ewq_fileio.name)
+        write(ewq_file, read(ewq_fileio))
+        close(zip_reader)
+
+        # test file contents
+        @test ncgetatt(ewq_file, "Global", "time_coverage_start") == "20710101"
+        @test ncgetatt(ewq_file, "Global", "time_coverage_end") == "21001231"
+        @test ncgetatt(ewq_file, "Global", "invar_experiment_name") == "rcp26"
+
+        # cleanup
+        rm(filepath)
+        rm(ewq_file)
     end
 end
