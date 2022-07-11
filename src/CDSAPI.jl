f3e99e88b0f5b7d1e14a6da3b5cb7b743b898c32
Alexander Barth <barth.alexander@gmail.com>
2022-07-11 13:49:35 +0200

    reduce screen output and less polling on the server (#41)
    * address JuliaClimate/CDSAPI.jl/issues/40
    
    * allow version HTTP v1
    
    * error out if request fails

--- src/CDSAPI.jl
+++ src/CDSAPI.jl
@@ -8,13 +8,16 @@ export
     py2ju
 
 """
-    retrieve(name, params, filename)
+    retrieve(name, params, filename; max_sleep = 120.)
 
 Retrieves data for `name` from the Climate Data Store
 with the specified `params` and stores it in the current
 directory as `filename`.
+
+The client periodically requests the status of the retrieve request.
+`max_sleep` is the maximum time (in seconds) between the status updates.
 """
-function retrieve(name, params, filename)
+function retrieve(name, params, filename; max_sleep = 120.)
     creds = Dict()
     open(joinpath(homedir(),".cdsapirc")) do f
         for line in readlines(f)
@@ -33,10 +36,23 @@ function retrieve(name, params, filename)
 
     resp_dict = JSON.parse(String(response.body))
     data = Dict("state" => "queued")
+    sleep_seconds = 1.
+
     while data["state"] != "completed"
         data = HTTP.request("GET", creds["url"] * "/tasks/" * string(resp_dict["request_id"]),  ["Authorization" => apikey])
         data = JSON.parse(String(data.body))
         println("request queue status ", data["state"])
+
+        if data["state"] == "failed"
+            error("Request to dataset $name failed. Check " *
+                  "https://cds.climate.copernicus.eu/cdsapp#!/yourrequests " *
+                  "for more information (after login).")
+        end
+
+        sleep_seconds = min(1.5 * sleep_seconds,max_sleep)
+        if data["state"] != "completed"
+            sleep(sleep_seconds)
+        end
     end
 
     HTTP.download(data["location"], filename)
