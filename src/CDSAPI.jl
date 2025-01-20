8f1df94ffb438c34e2474847de2c49b7e0fdbbbe
Camillo Schenone <camillo.schenone@gmail.com>
2025-01-20 16:08:15 +0100

    fix: remove gimmick progress bar, expand Dates compat version.

--- src/CDSAPI.jl
+++ src/CDSAPI.jl
@@ -62,11 +62,8 @@ function retrieve(name, params::AbstractDict, filename; wait=1.0)
         data = JSON.parse(String(data.body))
 
         if data["status"] != laststatus
-            print("\e[1K\e[0E") # erase terminal line and reset cursor
             @info "$(Dates.format(now(), dateformat"HH:MM:SS")) - CDS request" dataset=name status=data["status"]
             laststatus = data["status"]
-        else
-            print('.')
         end
 
 
@@ -90,7 +87,6 @@ function retrieve(name, params::AbstractDict, filename; wait=1.0)
     )
     body = JSON.parse(String(response.body))
 
-    print("\e[1K\e[0E")
     HTTP.download(body["asset"]["value"]["href"], filename)
 
     return data
